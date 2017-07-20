0354412527b73ebdeae9254c451c69f47fd75bca
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-07-20 16:29:26 -0700

    Build out route logic

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,87 @@
 const express = require('express')
 const router = express.Router()
+const authHelpers = require('./auth_helpers')
+const db = require('../db/queries')
+const signupSchema = require('./validations/signupSchema')
+const loginSchema = require('./validations/loginSchema')
 
 router.get('/', (req, res) => {
-  res.render('home', { email: req.session.email })
+  res.render('home', { email: req.session.email, message: req.session.message })
 })
 
 router.get('/login', (req, res) => {
   if (req.session.email) {
     res.redirect('/')
   }
-  res.render('login', { success: req.session.success, errors: req.session.errors })
+  res.render('login', { errors: req.session.errors, message: req.session.message })
   req.session.errors = null
+  req.session.message = null
 })
 
 router.get('/signup', (req, res) => {
-  res.render('signup', { success: false, errors: req.session.errors })
+  if (req.session.email) {
+    res.redirect('/')
+  }
+  res.render('signup', { errors: req.session.errors })
   req.session.errors = null
+  req.session.message = null
 })
 
-router.post('/login', (req, res) => {
-  req.check('email', 'Please provide a valid email').isEmail()
-  req.check('password', 'Password must be between 5 - 12 characters').isLength({ min: 5, max: 12})
+router.post('/login', (req, res, next) => {
+  const { email, password } = req.body
+  req.checkBody(loginSchema)
+
+  req.getValidationResult()
+    .then(errors => {
+      if (!errors.isEmpty()) {
+        req.session.errors = errors.array()
+        res.redirect('/login')
+      } else {
+        db.findUserByEmail(email)
+          .then(foundUser => {
+            req.session.errors = null
+            if (foundUser.length === 0) {
+              req.session.message = 'No record of those login credentials'
+              res.redirect('/login')
+            } else {
+              if (!authHelpers.comparePasswords(password, foundUser[0].password)) {
+                req.session.message = 'Incorrect Password!'
+                res.redirect('/login')
+              } else {
+                req.session.email = email
+                req.session.message = 'You are successfully logged in! '
+                res.redirect('/')
+              }
+            }
+          })
+          .catch(error => {
+            res.status(500).render('error', { error })
+          })
+      }
+    })
+  })
+
+router.post('/signup', (req, res, next) => {
+  req.checkBody(signupSchema)
   req.check('password', 'Passwords must match').equals(req.body.password2)
 
   req.getValidationResult()
     .then(errors => {
       if (!errors.isEmpty()) {
         req.session.errors = errors.array()
-        req.session.success = false
-        res.redirect('/login')
+        res.redirect('/signup')
       } else {
-        req.session.email = req.body.email
-        req.session.success = true
-        res.redirect('/')
+        db.createNewUser(req.body)
+          .then(result => {
+            req.session.email = result.email
+            req.session.message = 'Thank you for signing up. You are now logged in!'
+            res.redirect('/')
+          })
+          .catch(error => {
+            res.status(500).render('error', { error })
+          })
       }
     })
-})
-
-router.post('/signup', (req, res) => {
-
 })
 
 router.post('/logout', (req, res) => {
